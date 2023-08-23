dc98029f4ccd5afb2decb49fb5d5fc1121509a7e
Abhishek Dhawale <abhishek.dhawale@capgemini.com>
2023-08-23 22:30:24 +0530

    Epi 03

--- App.js
+++ App.js
@@ -1,49 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    'div',
-    {id: 'parent'},
-    [React.createElement(
-        'div',
-        {id: 'child1'},
-        [React.createElement(
-            'h1', 
-            {id: 'child-heading-1'}, 
-            'Hello World From React!!! child 1 using app.js'
-        ),
-        React.createElement(
-            'h2', 
-            {id: 'child-heading-2'}, 
-            'Hello World From React!!! child 2 using app.js'
-        )]
-    ),
-    React.createElement(
-        'div',
-        {id: 'child2'},
-        [React.createElement(
-            'h1', 
-            {id: 'child-heading-3'}, 
-            'Hello World From React!!! child 1 using app.js'
-        ),
-        React.createElement(
-            'h2', 
-            {id: 'child-heading-4'}, 
-            'Hello World From React!!! child 2 using app.js'
-        )]
-    )]
-);
-console.log(parent);
-
-
-const heading = React.createElement(
-    'h1', 
-    {id: 'heading', xyz: 'abhi'}, 
-    'Hello World From React!!! using app.js'
+// react element
+const title = (
+    <h1 id="heading">
+        Namaste React using JSX
+    </h1>
 );
 
-console.log(heading); // object
+// react functional components  // Component composition
+const HeadingCompmponent = () => (
+    <div className="container">
+        {title}
+        <h1>Namaste React functional components</h1>
+    </div>
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingCompmponent/>);
+
+
